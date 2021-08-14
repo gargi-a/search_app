af52049d80a0fd823fe11c8e27174c3e3601ed6d
Gargi Andhale <86964866+gargi-a@users.noreply.github.com>
2021-08-14 22:58:53 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,62 @@
+import img1 from './img/img1.jpg'
+import img2 from './img/img2.jpg'
+import img3 from './img/img3.jpg'
+import img4 from './img/img4.jpg'
+import img5 from './img/img5.jpg'
+import img6 from './img/img6.jpg'
+import img7 from './img/img7.jpg'
+import img8 from './img/img8.jpg'
+
+const data = {
+    cardData: [
+        {
+            id: 1,
+            img: img1,
+            title: 'Geralt of Rivia',
+            desc: 'White Wolf'
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Yennefer',
+            desc: 'Horsewoman of War'
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Dandelion',
+            desc: 'Jaskier'
+        },
+        {
+            id: 4,
+            img: img4,
+            title: 'Tissaia',
+            desc: 'Skylark'
+        },
+        {
+            id: 5,
+            img: img5,
+            title: 'Queen Calanthe',
+            desc: 'Lioness of Cintra'
+        },
+        {
+            id: 6,
+            img: img6,
+            title: 'Mousesack',
+            desc: 'Ermion'
+        },
+        {
+            id: 7,
+            img: img7,
+            title: 'Renfri',
+            desc: 'the Hexer'
+        },
+        {
+            id: 8,
+            img: img8,
+            title: 'Princess Cirilla',
+            desc: 'Lion Cub of Cintra'
+        }
+    ]
+}
+export default data
